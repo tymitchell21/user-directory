ebb2509f8a5fed199f76d69667b694effff68410
Tyler Ward <tymitchell21@icloud.com>
2018-05-14 16:58:20 -0400

    Multiple headings, button changes second one

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('button.greeting')
 
 function changeText(ev) {
-    const heading = document.querySelector('h1')
+    const heading = document.querySelector('h1.heading2')
     heading.innerText = "I am a G"
 }
 
