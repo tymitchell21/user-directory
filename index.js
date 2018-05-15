682d8aed58846b406543d9538340aad4a1365195
Tyler Ward <tymitchell21@icloud.com>
2018-05-15 15:45:39 -0400

    Display users in their favorite colors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const handleSubmit = function(ev) {
     const users = document.querySelector('#users')
     const name = ev.target.name.value
     const age = ev.target.age.value
+    const color = ev.target.favoriteColor.value
 
     const p = document.createElement('p');
     p.textContent = `${name}, ${age}`
@@ -14,8 +15,9 @@ const handleSubmit = function(ev) {
     ev.target.reset()
     ev.target.name.focus()
 
-    document.body.style.backgroundColor = "red"
+    //document.body.style.backgroundColor = color
     document.body.style.fontFamily = "Impact, Charcoal, sans-serif"
+    p.style.backgroundColor = color
     document.getElementById('girlfriend').style = "display: block"
 }
 
