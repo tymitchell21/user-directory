2c092a647038c85f274e0f84df4b3a91a371d453
Tyler Ward <tymitchell21@icloud.com>
2018-05-15 16:14:54 -0400

    Display color in an empty div

--- index.js
+++ index.js
@@ -13,18 +13,26 @@ const handleSubmit = function(ev) {
     const ageItem = document.createElement('li')
     ageItem.textContent = `Age: ${age}`
 
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = color
+    colorDiv.style.width = '6rem'
+    colorDiv.style.height = '3rem'
+
     const colorItem = document.createElement('li')
+    colorItem.textContent = 'Favorite Color:'
+    colorItem.appendChild(colorDiv)
 
     const list = document.createElement('ul')
     list.appendChild(nameItem)
-    list.appendChild(ageItem)    
+    list.appendChild(ageItem)
+    list.appendChild(colorItem)  
 
     users.appendChild(list)
 
     ev.target.reset()
     ev.target.name.focus()
 
-    document.body.style.backgroundColor = color
+    //document.body.style.backgroundColor = color
     document.body.style.fontFamily = "Impact, Charcoal, sans-serif"
     //p.style.backgroundColor = color
     document.getElementById('girlfriend').style = "display: block"
