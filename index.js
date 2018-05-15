ac06da0847f823265e2436c80bffcf60112d9043
Tyler Ward <tymitchell21@icloud.com>
2018-05-15 09:11:57 -0400

    Super duper Bonus and then some

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const button = document.querySelector('button.greeting')
 
 function changeText() {
     const heading = document.querySelector('h1.heading2')
-    heading.innerText = document.getElementById('text').value.toString()
+    heading.innerText = document.getElementById('text').value
+    document.body.style.backgroundColor = "red"
+    document.body.style.fontFamily = "Impact, Charcoal, sans-serif"
+    document.getElementById('girlfriend').style = "display: block"
 }
 
 button.addEventListener('click', changeText)
