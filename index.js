8808f347dd16319d08378c2df0886dc9554a3967
Tyler Ward <tymitchell21@icloud.com>
2018-05-16 15:08:28 -0400

    Extract renderList Function

--- index.js
+++ index.js
@@ -3,24 +3,38 @@ const form = document.querySelector('#userForm')
 //Submits form and creates a new unordered list by calling other functions
 const handleSubmit = function(ev) {
     ev.preventDefault()
-    const users = document.querySelector('#users')
-    const name = ev.target.name.value
-    const age = ev.target.age.value
-    const color = ev.target.favoriteColor.value
 
-    users.appendChild(renderList(name, age, color))
-    users.style.float = "center"
+    const f = ev.target
+    const user = {
+        userName: f.name.value,
+        age: f.age.value,
+        color: renderColor(f.favoriteColor.value),
+    }
+
+    const users = document.querySelector('#users')
+    users.appendChild(buildList(user))
 
     ev.target.reset()
     ev.target.name.focus()
 
-    document.getElementById('newContent').style.backgroundColor = color
+    //document.getElementById('newContent').style.backgroundColor = user[color]
     document.body.style.fontFamily = "Impact, Charcoal, sans-serif"
     //p.style.backgroundColor = color
     document.getElementById('girlfriend').style = "display: block"
 }
 
 form.addEventListener('submit', handleSubmit)
+
+function buildList(data) {
+    const list = document.createElement('ul')
+
+    const labels = Object.keys(data)
+    labels.forEach(function(label) {
+        const item = renderListItem(label, data[label])
+        list.appendChild(item)
+    })
+    return list
+}
 
 //Creates a div, gives it a background color, height and width, and then returns it
 function renderColor(color) {
